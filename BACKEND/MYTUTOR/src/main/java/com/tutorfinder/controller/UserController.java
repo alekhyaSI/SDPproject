30e494022c82416d34b768ae92b3a749b08fe781
lasya06-tech <2300030060cseh@gmail.com>
2025-09-27 03:10:29 +0530

    updated all codes

--- BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/UserController.java
+++ BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //final project
+//new final project
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("*") // allow frontend urls all
