1eac1052b4b1944e450899a6cfb4c21eb28bffe0
alekhyaSI <alekhya.bandaru14@gmail.com>
2025-10-05 22:53:09 +0530

    project

--- FRONTEND/tutor/src/components/AdminDashBoard.jsx
+++ FRONTEND/tutor/src/components/AdminDashBoard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./AdminDashboard.css";
+import "./AdminDashBoard.css";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
