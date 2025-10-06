d7196f13420dc4181563365ec208e9f009f42533
alekhyaSI <alekhya.bandaru14@gmail.com>
2025-10-06 10:53:59 +0530

    updated

--- BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/BookingController.java
+++ BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/BookingController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/bookings")
 @CrossOrigin(origins = "*")
 public class BookingController {
 
--- BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/TutorProfileController.java
+++ BACKEND/MYTUTOR/src/main/java/com/tutorfinder/controller/TutorProfileController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/tutors/profile")
+@RequestMapping("/tutors/profile")
 @CrossOrigin(origins = "*")
 public class TutorProfileController {
 
