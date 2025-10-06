41d6f0d93991f6ab303201b25d3c479aa9b2d4c7
alekhyaSI <alekhya.bandaru14@gmail.com>
2025-10-06 15:38:28 +0530

    project updated

--- BACKEND/MYTUTOR/src/main/java/com/tutorfinder/config/GlobalCorsConfig.java
+++ BACKEND/MYTUTOR/src/main/java/com/tutorfinder/config/GlobalCorsConfig.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class GlobalCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-    	registry.addMapping("/**")  // <-- allow all paths
-        .allowedOrigins("http://localhost:5173")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowCredentials(true)
-        .maxAge(3600);
+        registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+                        .maxAge(3600);
 
     }
 }
